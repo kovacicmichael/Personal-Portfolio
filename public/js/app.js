65b65eaade90f05c58d1833fbae1c3e1132920f6
Michael Kovacic <michael.j.kovacic@gmail.com>
2018-05-01 16:03:21 -0600

    home page complete

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,9 @@
+ 
  $("document").ready(function(){
- 	$("#sidebar-wrapper").toggleClass("active");
- 	
+ 	//$("#sidebar-wrapper").toggleClass("active");
+ 	$(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass("fa-bars fa-times");
+
+ 	moveArrow();
  })
 
  // Closes the sidebar menu
@@ -9,7 +12,25 @@
     $("#sidebar-wrapper").toggleClass("active");
     $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass("fa-bars fa-times");
     $(this).toggleClass("active");
+
+    moveArrow();
   });
+
+function moveArrow(){
+	if($(".active")[0]){
+ 		console.log("active")
+ 		$(".arrowContent").css("top", '40%');
+ 		$(".arrowContent").css("right", '7%');
+ 		$(".arrow").css("transform", "rotate(-108deg)");
+ 		$(".arrowContent").css("transform", "scaleX(1)");
+ 	}else{
+ 		console.log("notactive")
+ 		$(".arrowContent").css("top", '4%');
+ 		$(".arrowContent").css("right", '-17%');
+ 		$(".arrowContent").css("transform", "scaleX(-1)");
+ 		$(".arrow").css("transform", "rotate(132deg)");
+ 	}
+}
 
 $("#button").on("click", function(){
 	console.log("click")
@@ -24,5 +45,5 @@ function openNav() {
 /* Set the width of the side navigation to 0 */
 function closeNav() {
 	console.log("close")
-    document.getElementById("mySidenav").style.width = "0";
+    document.getElementByClassName("arrowContent")[0].style.top = "10%";
 }
